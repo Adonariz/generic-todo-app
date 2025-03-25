ef6a252a8af87d57a7b66c4ef4c075acb4f0c855
Adonariz <raikkonen07@live.ru>
2025-03-25 13:01:51 +0300

    подключает tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
